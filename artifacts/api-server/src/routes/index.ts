95bd3864f6c1f0745f1df16dd39db9c3822ba37a
Brandon1212b <111893060+Brandon1212b@users.noreply.github.com>
2026-06-29 19:02:10 -0400

    Fix import and usage of oddsRouter

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,10 +1,10 @@
-@@
+
  import smackboardRouter from "./smackboard";
  import adminRouter from "./admin";
-+import oddsRouter from "./odds";
-@@
+ import oddsRouter from "./odds";
+
  router.use(smackboardRouter);
  router.use(adminRouter);
-+router.use(oddsRouter);
+ router.use(oddsRouter);
  
  export default router;
